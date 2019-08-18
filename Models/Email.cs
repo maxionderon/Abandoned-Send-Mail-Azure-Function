bbfa3c673a4badb3444ccbfc96e29e42d0c8a2a0
maxionderon <ch-mp@web.de>
2019-08-18 17:45:28 +0200

    Implemented -Model -Function -added SendGrid dependency -costum route and api endpoint

---
+++ Models/Email.cs
@@ -0,0 +1,17 @@
+namespace Maxionderon.Function.Models
+{
+
+    public class Email
+    {
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+
+        
+
+    }
+
+}
--- sendMailFunction.cs
+++ sendMailFunction.cs
@@ -8,19 +8,63 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
+using System.Collections.Generic;
+
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+
+using Maxionderon.Function.Models;
+
 namespace Maxionderon.Function
 {
     public static class sendMailFunction
     {
         [FunctionName("sendMailFunction")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "mail")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            return new OkObjectResult($"Hello World");
+            req.Headers.ToString();
+           
+            try {
+
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                Email email = JsonConvert.DeserializeObject<Email>(requestBody);
+
+                SendGridMessage sendGridMessage = new SendGridMessage();
                 
+                sendGridMessage.SetFrom(email.emailAddress, email.lastName + ", " + email.firstName);
+                
+                List<EmailAddress> myMails = new List<EmailAddress>();
+
+                myMails.Add(new EmailAddress(Environment.GetEnvironmentVariable("EMAIL1") , Environment.GetEnvironmentVariable("NAME")));
+                myMails.Add(new EmailAddress(Environment.GetEnvironmentVariable("EMAIL2") , Environment.GetEnvironmentVariable("NAME")));
+                myMails.Add(new EmailAddress(Environment.GetEnvironmentVariable("EMAIL3") , Environment.GetEnvironmentVariable("NAME")));
+
+                sendGridMessage.AddTos(myMails);
+                sendGridMessage.SetSubject(email.subject);
+                sendGridMessage.PlainTextContent = email.message;
+
+                string apiKey = System.Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
+                SendGridClient sendGridClient = new SendGridClient(apiKey);
+
+                await sendGridClient.SendEmailAsync(sendGridMessage);
+
+                return new OkObjectResult("email was send");
+
+            }catch(Exception e) {
+
+                log.LogInformation(e.ToString());
+
+                return new BadRequestObjectResult("Bad Request");  
+
+            }          
+
+              
         }
+
     }
 }
